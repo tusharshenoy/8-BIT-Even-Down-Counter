15ef5f8a4c050e6317af0a3e62c1c125eb9771b1
T Tushar Shenoy <107348474+tusharshenoy@users.noreply.github.com>
2023-08-27 16:49:05 +0530

    Create df.v

---
+++ df.v
@@ -0,0 +1,15 @@
+module df(d,q,qb,clk,rst);
+  input d;
+  input rst;
+  input clk;
+  output q,qb;
+  reg q,qb;
+    always@(posedge clk, posedge rst)
+      begin
+        if(rst)
+          q=1'b0;
+        else
+          q=d;
+          qb=~q;
+        end
+endmodule
